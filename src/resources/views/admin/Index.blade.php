81f34bdb35ef629cac6c47569d2625fee9de80d3
julian bueno <jdbuenol@pop-os.localdomain>
2022-03-26 19:57:02 -0500

    Admin Dashboard

--- src/resources/views/admin/Index.blade.php
+++ src/resources/views/admin/Index.blade.php
@@ -2,5 +2,32 @@
 @extends('layouts.AdminApp')
 @section('title', "ADMIN PANEL")
 @section('content')
-THIS IS THE DASHBOARD
+<div class="PostsHeader">
+    <diV class="DashboardGrid">
+        <a href="{{ route('admin.users') }}" class="GridSquare"><div>
+            <p class="display-3">USERS</p>
+        </div></a>
+        <a href="{{ route('admin.orders') }}" class="GridSquare"><div>
+            <p class="display-3">ORDERS</p>
+        </div></a>
+        <a href="{{ route('admin.fishOrders') }}" class="GridSquare"><div>
+            <p class="display-3">ITEMS</p>
+        </div></a>
+        <a href="{{ route('admin.foodFishes') }}" class="GridSquare"><div>
+            <p class="display-3">FOOD FISHES</p>
+        </div></a>
+        <a href="{{ route('admin.petFishes') }}" class="GridSquare"><div>
+            <p class="display-3">PET FISHES</p>
+        </div></a>
+        <a href="{{ route('admin.species') }}" class="GridSquare"><div>
+            <p class="display-3">SPECIES</p>
+        </div></a>
+        <a href="{{ route('admin.locations') }}" class="GridSquare"><div>
+            <p class="display-3">LOCATIONS</p>
+        </div></a>
+        <a href="{{ route('admin.speciesLocations') }}" class="GridSquare"><div>
+            <p class="display-3">SPECIES LOCATIONS</p>
+        </div></a>
+    </diV>
+</div>
 @endsection
